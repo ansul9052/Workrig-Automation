8573e8bb05ad6bb68bac72ca020742be78c781be
Anshulgaur <146942653+Anshulgaur@users.noreply.github.com>
2024-05-19 16:43:18 +0530

    new fix

--- src/test/java/WorkrigAutoamation/Workrig_Autoamtion.java
+++ src/test/java/WorkrigAutoamation/Workrig_Autoamtion.java
@@ -4,62 +4,51 @@ import java.time.Duration;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-//import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-//import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class Workrig_Autoamtion {
+public class Workrig_Autoamtion  {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeMethod
-    public void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-
-       // WebDriverManager.chromedriver().setup();
-    	System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver.exe");
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
-    @Test()
-    public void loginAndLogoutTest() throws InterruptedException {
-    	driver.get("https://quloi.myworkrig.com/");
-        
+    @Test
+    public void loginAndCheckInOut() throws InterruptedException {
+        driver.get("https://quloi.myworkrig.com/");
+
         WebElement username = driver.findElement(By.xpath("//input[@id='form-username']"));
         WebElement password = driver.findElement(By.xpath("//input[@id='form-password']"));
-       // WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        WebElement checkInOutButton = driver.findElement(By.cssSelector(".btn.btn-info"));
 
         username.sendKeys("anshul.gaur");
         password.sendKeys("vd8m5791");
-
-        
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("footer.main-footer")));
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
         loginButton.click();
         Thread.sleep(5000);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn.btn-info"))).click();
-        
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn.btn-info")));
+        checkInOutButton.click();
+
         Alert alert = driver.switchTo().alert();
         alert.accept();
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 }
